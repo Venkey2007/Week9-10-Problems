777385e4bb75b4b65906c27565884aa7d4dc2fc5
Venkey2007 <venkateswarareddythatigotla@gmail.com>
2026-04-29 15:27:54 +0530

    Added string to char array comparison program

---
+++ StringToCharArrayComparison.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class StringToCharArrayComparison {
+
+    public static char[] getCharacters(String text) {
+        char[] chars = new char[text.length()];
+
+        for (int i = 0; i < text.length(); i++) {
+            chars[i] = text.charAt(i);
+        }
+
+        return chars;
+    }
+
+    public static boolean compareCharArrays(char[] arr1, char[] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = scanner.next();
+
+        char[] customArray = getCharacters(text);
+        char[] builtInArray = text.toCharArray();
+
+        boolean result = compareCharArrays(customArray, builtInArray);
+
+        System.out.println("Comparison Result: " + result);
+    }
+}
+
